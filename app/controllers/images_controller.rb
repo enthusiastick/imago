b27f496d47a3e2e0bff0c586ad712ec46d2de2da
Eben <eben.lowe@gmail.com>
2014-02-03 16:20:15 -0500

    Happy path for images working; piece has many images.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,26 @@
 class ImagesController < ApplicationController
 
+  def create
+    @image = Image.new(image_params)
+    @image.piece = Piece.find(params[:piece_id])
+    if @image.save
+      flash[:notice] = "Image uploaded successfully."
+      redirect_to piece_path(@image.piece)
+    else
+      flash[:error] = "Error! Haven't you learned anything?"
+      render :new
+    end
+  end
+
   def new
+    @piece = Piece.find(params[:piece_id])
     @image = Image.new
+  end
+
+  protected
+
+  def image_params
+    params.require(:image).permit(:caption, :piece)
   end
 
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ActiveRecord::Base
 
-  mount_uploader :url, ImageUploader
+  mount_uploader :img, ImageUploader
 
   belongs_to :piece
 
