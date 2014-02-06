31905972dcd51a651b96c3cf07bd0210623a28a5
Eben <eben.lowe@gmail.com>
2014-02-06 18:54:47 -0500

    Set weight after create to default to row id; resolved bug where update button was calling delete action.

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,12 @@ class Image < ActiveRecord::Base
   mount_uploader :img, ImageUploader
   validates_numericality_of :weight, integer_only: true
 
+  after_create :set_weight
+
+  def set_weight
+    self.update(weight: self.id) if self.weight == 0
+  end
+
   belongs_to :piece
 
 end
