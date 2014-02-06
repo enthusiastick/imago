2d4170f9485047273c139be5ed4df730b69b8361
Eben <eben.lowe@gmail.com>
2014-02-06 16:12:57 -0500

    Re-factored tests.

--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -2,8 +2,7 @@ module LoginHelper
   def login(user)
     # user.confirmed_at = Time.now
     user.save
-    visit '/'
-    click_on 'Sign In'
+    visit new_user_session_path
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     click_on 'Sign in'
