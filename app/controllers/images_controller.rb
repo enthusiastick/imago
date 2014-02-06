ce11c2a072f79d71591cacfe701048a686bdf766
Eben <eben.lowe@gmail.com>
2014-02-06 16:11:02 -0500

    Update added to controller, feature happy path working.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -21,10 +21,25 @@ class ImagesController < ApplicationController
     @image = Image.find(params[:id])
   end
 
+  def update
+    @image = Image.find(params[:id])
+    if @image.update(update_params)
+      flash[:success] = "Image info updated successfully."
+      redirect_to piece_path(@image.piece)
+    else
+      flash[:alert] = "Error! Unable to update image info."
+      redirect_to piece_path(@image.piece)
+    end
+  end
+
   protected
 
   def image_params
     params.require(:image).permit(:caption, :piece, :img)
+  end
+
+  def update_params
+    params.require(:image).permit(:caption, :weight)
   end
 
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,7 @@
 class Image < ActiveRecord::Base
 
   mount_uploader :img, ImageUploader
+  validates_numericality_of :weight, integer_only: true
 
   belongs_to :piece
 
