5a7bab41ebe17fd51af607b47547575e007ed440
Eben <eben.lowe@gmail.com>
2014-02-06 23:32:03 -0500

    Changed default weight after save from id to id x 10.

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,7 +6,7 @@ class Image < ActiveRecord::Base
   after_create :set_weight
 
   def set_weight
-    self.update(weight: self.id) if self.weight == 0
+    self.update(weight: (self.id*10)) if self.weight == 0
   end
 
   belongs_to :piece
